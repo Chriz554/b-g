99f1c5c3e1a2098c91cdcfdb4790177827e77efd
Christopher Zuñiga Garcia <2022371180@uteq.edu.mx>
2025-11-20 18:35:04 -0600

    Update deploy script for app_blue
    Fix docker build context path and add echo statement.

--- scripts/deploy_blue.sh
+++ scripts/deploy_blue.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker build -t app_blue ..
+docker build -t app_blue .
 docker stop app_blue || true
 docker rm app_blue || true
+
 docker run -d --name app_blue -p 3001:3000 app_blue
+echo "BLUE ready on port 3001"
