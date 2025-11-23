1d659f86f46d2ce395866953845c1a0cc4acc8fb
Christopher Zuñiga Garcia <2022371180@uteq.edu.mx>
2025-11-23 15:30:23 -0600

    Refactor switch.sh to remove green option
    Removed unused color options and simplified script.

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -2,18 +2,9 @@
 COLOR=$1
 
 if [ "$COLOR" = "blue" ]; then
-<<<<<<< HEAD
     sed -i 's/server 127.0.0.1:9081/server 127.0.0.1:9080/' /etc/nginx/conf.d/default.conf
 else
     sed -i 's/server 127.0.0.1:9080/server 127.0.0.1:9081/' /etc/nginx/conf.d/default.conf
-=======
-    NEW_LINE="server 127.0.0.1:3001;"
-elif [ "$COLOR" = "green" ]; then
-    NEW_LINE="server 127.0.0.1:3002;"
-else
-    echo "Uso: switch.sh [blue|green]"
-    exit 1
->>>>>>> e1b59b4b3440297f57f563922c6e9b2f1f34733c
 fi
 
 sudo sed -i "s|#ACTIVE_APP|$NEW_LINE|" /etc/nginx/conf.d/default.conf
