5094d4cffaeec46b90ac186ddbec353b797bab30
Christopher Zuñiga Garcia <2022371180@uteq.edu.mx>
2025-11-25 21:07:41 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola! Soy la versión Blue-Green funcionando - Prueba de cambio entre colores AHHHH");
+  res.send("Hola! Soy la versión Blue-Green funcionando - Prueba de cambio entre colores AHHHH HOLAAAAAA");
 });
 
 app.listen(3000, () => console.log("App running on port 3000"));
